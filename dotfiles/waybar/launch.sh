4b274b5e54fbad865aab1ab15cd9d77d6ad9ff90
leanderkuehnel0 <leander.kuehnel@mail.de>
2026-03-22 11:08:50 +0100

    update

--- dotfiles/waybar/launch.sh
+++ dotfiles/waybar/launch.sh
@@ -1,6 +1,4 @@
 pkill waybar
 export CONFIG_PATH="$HOME/.config/waybar/"
 $CONFIG_PATH/pywal_background.py
-sleep 2
-pkill waybar
 waybar -c $CONFIG_PATH/$1/$(cat $CONFIG_PATH/current_theme)/config -s $CONFIG_PATH/$1/$(cat $CONFIG_PATH/current_theme)/style.css >> $CONFIG_PATH/log
