ccefaa60996cf874ad86f61b45091d6775ef3ba8
leanderkuehnel0 <leander.kuehnel@mail.de>
2026-03-22 11:07:59 +0100

    update

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo xbps-install swayfx cliphist wl-clipboard grim geeqie slurp python3-pip pywal wofi foot bat firefox Waybar pipewire wireplumber pavucontrol bluez bluetuith libspa-bluetooth xdg-utils xdg-user-dirs elogind wlroots zenity
+sudo ln -s /etc/sv/dbus /var/service/
+
+sudo xbps-install mesa-dri
+pip install colorz pywalfox --break-system-packages
+
